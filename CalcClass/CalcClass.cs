07766cd190866946211468442cc4d6b6a4e791e9
Ievgen Chernat <ievgenchernat@gmail.com>
2022-04-14 19:44:13 +0300

    Added CalcClass

---
+++ CalcClass/CalcClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcClasss
+{
+    public class CalcClass
+    {
+        private static string _lastError = "";
+        public static string lastError;
+
+        public static double Add(long a, long b)
+        {
+            checked
+            {
+                return a + b;
+            }
+        }
+        public static double Sub(long a, long b)
+        {
+            checked
+            {
+                return a - b;
+            }
+        }
+        public static double Mult(long a, long b)
+        {
+            checked
+            {
+                return a * b;
+            }
+        }
+        public static double Div(long a, long b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException();
+            else
+                return a / b;
+        }
+        public static double Mod(long a, long b)
+        {
+            checked
+            {
+                return a % b;
+            }
+        }
+        public static double ABS(long a)
+        {
+            if (a < 0.0)
+            {
+                return -a;
+            }
+            return a;
+        }
+        public static double IABS(long a)
+        {
+            if (a > 0.0)
+            {
+                return a;
+            }
+            return -a;
+        }
+    }
+}
