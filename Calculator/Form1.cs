0e563a1ab12d0359e01e6672cc859bbb4cb2d4f2
Ievgen Chernat <ievgenchernat@gmail.com>
2022-04-14 21:59:38 +0300

    Добавлено обчислення виразу btn_equal_Click

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,11 @@ namespace Calculator
     public partial class Form1 : Form
     {
         double Memory;
+        private string _fix;
+        private double _nik;
+        private readonly string _undoArrow = char.ConvertFromUtf32(0x00002190);
+
+
         public Form1()
         {
             InitializeComponent();
@@ -168,11 +174,65 @@ namespace Calculator
             }
         }
 
-        
+        internal static bool CharIsAnOperator(string userInput)
+        {
+            return userInput.Equals("+") ||
+                   userInput.Equals("-") ||
+                   userInput.Equals("*") ||
+                   userInput.Equals("/") ||
+                   userInput.Equals("%");
+        }
+
+        private bool ParenthesesAmountIsEqual(string userInput)
+        {
+            int leftParenthesesAmount = 0;
+            int rightParenthesesAmount = 0;
+
+            foreach (var item in userInput)
+            {
+                if (item.Equals('('))
+                    leftParenthesesAmount++;
+                else if (item.Equals(')'))
+                    rightParenthesesAmount++;
+            }
+
+            return leftParenthesesAmount == rightParenthesesAmount;
+        }
+
         private void btn_equal_Click(object sender, EventArgs e)
         {
             //Обчислення
-            MessageBox.Show("Обчислення виразу");
+
+            if (CharIsAnOperator(text_Expression.Text[text_Expression.Text.Length - 1].ToString()) || (text_Expression.Text[text_Expression.Text.Length - 1].ToString().Equals(".")) || (text_Expression.Text[text_Expression.Text.Length - 1].ToString().Equals("(")))
+            {
+                MessageBox.Show(@"Error03! Please enter a valid character. 
+                    The last character of the equation cannot be an operator, a decimal separator, or a left parenthesis!",
+                    Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!ParenthesesAmountIsEqual(text_Expression.Text))
+            {
+                MessageBox.Show(@"Error01!!Please enter a valid character. 
+                The number of left and right parentheses is not the same!", 
+                Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _fix = text_Expression.Text;
+
+            try
+            {
+                Onp onp = new Onp(_fix);
+                _nik = onp.PostfixEvaluation(onp.InfixToPostfix());
+            }
+            catch
+            {
+                MessageBox.Show(@"A problem occured!", @"Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            text_Resualt.Text = _nik.ToString(CultureInfo.InvariantCulture).Replace(',', '.');
+
         }
 
 
@@ -215,14 +275,82 @@ namespace Calculator
         //Обробка натискання на кнопки з клавіатури
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Escape)
-            {
-                this.Close();
-            }
-            if(e.KeyCode == Keys.Enter)
-            {
-                MessageBox.Show("Обчислення виразу");
-            }
+            //if(e.KeyCode == Keys.Escape)
+            //{
+            //    this.Close();
+            //}
+            //if(e.KeyCode == Keys.Enter)
+            //{
+            //    MessageBox.Show("Обчислення виразу");
+            //}
+
+            if (e.KeyCode == Keys.Delete)
+                btn_C.PerformClick();
         }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar.ToString())
+            {
+                case "0":
+                    btn_0.PerformClick();
+                    break;
+                case "1":
+                    btn_1.PerformClick();
+                    break;
+                case "2":
+                    btn_2.PerformClick();
+                    break;
+                case "3":
+                    btn_3.PerformClick();
+                    break;
+                case "4":
+                    btn_4.PerformClick();
+                    break;
+                case "5":
+                    btn_5.PerformClick();
+                    break;
+                case "6":
+                    btn_6.PerformClick();
+                    break;
+                case "7":
+                    btn_7.PerformClick();
+                    break;
+                case "8":
+                    btn_8.PerformClick();
+                    break;
+                case "9":
+                    btn_9.PerformClick();
+                    break;
+                case "+":
+                    btn_plus.PerformClick();
+                    break;
+                case "-":
+                    btn_sub.PerformClick();
+                    break;
+                case "*":
+                    btn_mul.PerformClick();
+                    break;
+                case "/":
+                    btn_div.PerformClick();
+                    break;
+                case "(":
+                    btn_OpenArc.PerformClick();
+                    break;
+                case ")":
+                    btn_CloseArc.PerformClick();
+                    break;
+                case "=":
+                    btn_equal.PerformClick();
+                    break;
+            }
+
+            if (e.KeyChar.Equals((char)Keys.Enter))
+                btn_equal.PerformClick();
+
+            if (e.KeyChar.Equals((char)Keys.Back))
+                btn_C.PerformClick();
+        }
+
     }
 }
---
+++ Calculator/Onp.cs
@@ -0,0 +1,168 @@
+﻿using CalcClasss;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    class Onp
+    {
+        private readonly string _userInput;
+
+        public Onp(string userInput)
+        {
+            _userInput = userInput;
+        }
+
+        public Queue<string> InfixToPostfix()
+        {
+            StringBuilder postfixQueueCandidate = new StringBuilder();
+            Stack<char> specialTokensStack = new Stack<char>();
+            Queue<string> postfixQueue = new Queue<string>();
+            string infix = InfixConversion(_userInput);
+
+            foreach (char item in infix)
+            {
+                if (Char.IsNumber(item) || item.Equals('.'))
+                    postfixQueueCandidate.Append(item);
+                else
+                {
+                    if (postfixQueueCandidate.Length != 0)
+                    {
+                        postfixQueue.Enqueue(postfixQueueCandidate.ToString());
+                        postfixQueueCandidate.Clear();
+                    }
+
+                    if (item.Equals('('))
+                    {
+                        specialTokensStack.Push(item);
+                        continue;
+                    }
+
+                    if (item.Equals(')'))
+                    {
+                        while (!specialTokensStack.Peek().Equals('('))
+                        {
+                            postfixQueue.Enqueue(specialTokensStack.Pop().ToString());
+                        }
+                        specialTokensStack.Pop();
+                        continue;
+                    }
+                    if (specialTokensStack.Count() == 0 || specialTokensStack.Peek().Equals('('))
+                        specialTokensStack.Push(item);
+                    else
+                    {
+                        if (TokenPriorityLevel(item) > TokenPriorityLevel(specialTokensStack.Peek()))
+                            specialTokensStack.Push(item);
+                        else
+                        {
+                            while (specialTokensStack.Count() != 0 && (TokenPriorityLevel(item) <= TokenPriorityLevel(specialTokensStack.Peek())))
+                            {
+                                postfixQueue.Enqueue(specialTokensStack.Pop().ToString());
+                            }
+                            specialTokensStack.Push(item);
+                        }
+                    }
+                }
+            }
+            if (postfixQueueCandidate.Length != 0)
+                postfixQueue.Enqueue(postfixQueueCandidate.ToString());
+
+            while (specialTokensStack.Count() != 0)
+                postfixQueue.Enqueue(specialTokensStack.Pop().ToString());
+
+            return postfixQueue;
+        }
+
+        public double PostfixEvaluation(Queue<string> postfix)
+        {
+            double x1 = 0, x2 = 0;
+            Stack<double> result = new Stack<double>();
+
+            while (postfix.Count != 0)
+            {
+                var temp = postfix.Dequeue();
+
+                if (temp.Equals("+") || temp.Equals("-") || temp.Equals("*") || temp.Equals("/") || temp.Equals("%"))
+                {
+                    try
+                    {
+                        x2 = result.Pop();
+                        x1 = result.Pop();
+                    }
+                    catch
+                    {
+                        Console.WriteLine(@"stack is empty");
+                    }
+                    switch (temp)
+                    {
+                        case "+":
+                            result.Push(CalcClass.Add(x1,x2));
+                            break;
+                        case "-":
+                            result.Push(CalcClass.Sub(x1, x2));
+                            break;
+                        case "*":
+                            result.Push(CalcClass.Mult(x1, x2));
+                            break;
+                        case "/":
+                            result.Push(CalcClass.Sub(x1, x2));
+                            break;
+                        case "%":
+                            result.Push(CalcClass.Mod(x1, x2));
+                            break;
+                    }
+                }
+                else
+                    result.Push(double.Parse(temp, System.Globalization.CultureInfo.InvariantCulture));
+            }
+
+            return Convert.ToDouble(result.Pop());
+        }
+
+        private int TokenPriorityLevel(char token)
+        {
+            Dictionary<char, int> priorityDict = new Dictionary<char, int> { { '(', 0 }, { '+', 1 }, { '-', 1 }, { '*', 2 }, { '/', 2 }, { '%', 2 } };
+            return (priorityDict[token]);
+        }
+
+        private string InfixConversion(string userInput)
+        {
+
+            ArrayList arrayList = new ArrayList(userInput.ToCharArray());
+
+            if (arrayList[0].Equals('-') && Char.IsNumber((char)arrayList[1]))
+            {
+                arrayList.Insert(0, 0);
+                arrayList.Insert(0, '(');
+
+                for (int i = 3; i < arrayList.Count - 1; i++)
+                {
+                    if (Form1.CharIsAnOperator(arrayList[i].ToString()))
+                    {
+                        arrayList.Insert(i, ')');
+                        break;
+                    }
+                }
+            }
+
+            if (arrayList[0].Equals('-') && arrayList[1].Equals('('))
+                arrayList.Insert(0, 0);
+
+            for (int i = 0; i < arrayList.Count - 3; i++)
+            {
+                if (arrayList[i].Equals('(') && arrayList[i + 1].Equals('-') && !Form1.CharIsAnOperator(arrayList[i + 2].ToString()))
+                    arrayList.Insert(i + 1, 0);
+            }
+
+            StringBuilder infixReady = new StringBuilder();
+
+            foreach (var item in arrayList)
+                infixReady.Append(item);
+
+            return infixReady.ToString();
+        }
+    }
+}
