196cb3498d477535042decf6fb0a09b99dc1bc22
Viktoria Sadivska <vika.sadivska@gmail.com>
2022-04-14 10:09:28 +0300

    added analaizerClass.dll

---
+++ AnalaizerClass/Analaizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AnalaizerClass
+{
+    public class Analaizer
+    {
+        private static int erposition = 0;
+        public static string expression = "";
+        public static bool ShowMessage = true;
+        public static bool CheckCurrency()
+        {
+            int n = 0;
+            foreach (var item in expression)
+            {
+                if
+            }
+        }
+        public static string Format() { }
+        public static System.Collections.ArrayList CreateStack()
+        {
+
+        }
+        public static string RunEstimate() { }
+        public static string Estimate() { }
+    }
+}
