2266795aa6fbc072d7805b8fb5c9a7db536e02ed
Susidko Vladislav <vladislavsus7@gmail.com>
2022-04-14 13:27:40 +0300

    Вираз, який передається з консолі Закоментував, бо чеоез це програма не запускається

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -14,7 +14,7 @@ namespace Calculator
         [STAThread]
         static void Main(string[] args)
         {
-            string expression = args[0];//Вираз, який передається з консолі
+            //string expression = args[0];//Вираз, який передається з консолі
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
