112464ac4aa70f7a369b8b928f7db4e25dfaabab
Susidko Vladislav <vladislavsus7@gmail.com>
2022-04-17 15:35:51 +0300

    Додав тест для CalcClasss

---
+++ CalcClasssTests/CalcTests.cs
@@ -0,0 +1,98 @@
+﻿using CalcClasss;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalcClasssTests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            double input1 = 2;
+            double input2 = 3;
+            double expected = 5;
+            double actual = CalcClass.Add(input1, input2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SubTest()
+        {
+            double input1 = 2;
+            double input2 = 3;
+            double expected = -1;
+            double actual = CalcClass.Sub(input1, input2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MulTest()
+        {
+            double input1 = 2;
+            double input2 = 3;
+            double expected = 6;
+            double actual = CalcClass.Mult(input1, input2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Div_10divide0_Exception()
+        {
+            double input1 = 10;
+            double input2 = 0;
+            double actual = CalcClass.Div(input1, input2);
+        }
+
+        [TestMethod]
+        public void DivTest()
+        {
+            double input1 = 10;
+            double input2 = 2;
+            double expected = 5;
+            double actual = CalcClass.Div(input1, input2);
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void ABSTestPlus()
+        {
+            double input = 10;
+            double expected = 10;
+            double actual = CalcClass.ABS(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ABSTestMinus()
+        {
+            double input = -2;
+            double expected = 2;
+            double actual = CalcClass.ABS(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IABSTest()
+        {
+            double input = 12;
+            double expected = 12;
+            double actual = CalcClass.IABS(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IABSTestMinus()
+        {
+            double input = -12;
+            double expected = 12;
+            double actual = CalcClass.IABS(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CalcClasssTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcClasssTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcClasssTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79dd528e-3ca9-4f31-93d0-5146088f6cac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
