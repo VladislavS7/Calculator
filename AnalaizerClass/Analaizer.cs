535723e8777812a7d0d3e8b4588673b03fa2ae1a
Viktoria Sadivska <vika.sadivska@gmail.com>
2022-04-18 09:08:04 +0300

    .

---
+++ AnalaizerClass/Analaizer.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CalcClasss;
+
+namespace AnalaizerClass
+{
+    public class Analaizer
+    {
+        private static int erposition = 0;
+        public static string expression = "";
+        public static bool ShowMessage = true;
+        public static bool CheckCurrency()
+        {
+            int n = 0;
+            foreach (var item in expression)
+            {
+                if (item == '(') ++n;
+                else if (item == ')') --n;
+            }
+
+            if (n == 0)
+                return true;
+            else if (n > 0)
+            {
+                erposition = expression.LastIndexOf('(');
+                return false;
+            }
+            else
+            {
+                erposition = expression.LastIndexOf(')');
+                return false;
+            }
+        }
+        public static string Format()
+        {
+            string result = "";
+            string op = "*/+-";
+            int countNumOp = 0;
+
+            if (op.Contains(expression[expression.Length - 1]))
+                return "&Error 05";
+            if (expression.Length > 65536)
+                return "&Error 07";
+
+
+            for (int i = 0; i < expression.Length; ++i)
+            {
+                if (expression[i] != ' ')
+                    result += expression[i];
+
+                if (op.Contains(expression[i]))
+                    ++countNumOp;
+
+                if (Char.IsDigit(expression[i]) && !Char.IsDigit(expression[i + 1]) || op.Contains(expression[i]))
+                    result += ' ';
+                else if (!op.Contains(expression[i]) && !Char.IsDigit(expression[i]) && expression[i] != ' ')
+                    return $"&Error 02 at <{i}>";
+                else if (op.Contains(expression[i]) && op.Contains(expression[i + 1]))
+                    return $"&Error 04 at <{i + 1}>";
+            }
+
+            for (int i = 0; i < result.Length; ++i)
+            {
+                if (expression[i] == '/' && expression[i + 1] == '0' && expression[i + 2] == ' ')
+                    return "&Error 9";
+            }
+
+            string[] numbers = Regex.Split(expression, @"\D+");
+            foreach (string value in numbers)
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    int i = int.Parse(value);
+                    if (i <= -2147483648 || i >= 2147483647)
+                        return "&Error 6";
+                }
+            }
+
+            countNumOp += numbers.Length;
+            if (countNumOp > 30)
+                return "&Error 08";
+
+            return result;
+        }
+        public static System.Collections.ArrayList CreateStack()
+        {
+            return new System.Collections.ArrayList();
+        }
+        public static string RunEstimate()
+        {
+            string[] expTokens = expression.Split(' ');
+            Stack<double> stack = new Stack<double>();
+            double number = 0;
+
+            foreach (string token in expTokens)
+            {
+                if (double.TryParse(token, out number))
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    switch (token)
+                    {
+                        case "*":
+                            {
+                                stack.Push(CalcClass.Mult(stack.Pop(), stack.Pop()));
+                                break;
+                            }
+                        case "/":
+                            {
+                                number = stack.Pop();
+                                stack.Push(CalcClass.Div(stack.Pop(), number));
+                                break;
+                            }
+                        case "+":
+                            {
+                                stack.Push(CalcClass.Add(stack.Pop(), stack.Pop()));
+                                break;
+                            }
+                        case "-":
+                            {
+                                number = stack.Pop();
+                                stack.Push(CalcClass.Sub(stack.Pop(), number));
+                                break;
+                            }
+                        default:
+                            return "error";
+                            break;
+                    }
+                }
+            }
+
+            return stack.Pop().ToString();
+        }
+        public static string Estimate()
+        {
+            if (CheckCurrency() == false)
+                throw new Exception($"Error 1 at <{erposition}>");
+
+            string result = Format();
+            if (result[0] == '&')
+                throw new Exception(result);
+
+            expression = result;
+            ArrayList stack = CreateStack();
+
+            expression = stack.ToString();
+            return RunEstimate();
+        }
+    }
+}
