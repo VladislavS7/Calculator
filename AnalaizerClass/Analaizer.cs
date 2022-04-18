289f9366c86200e6ff6e842870018f8f94a3567f
Fizzyy <vip.mkola@gmail.com>
2022-04-18 08:45:50 +0300

    final work on Analaizer class

--- AnalaizerClass/Analaizer.cs
+++ AnalaizerClass/Analaizer.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace AnalaizerClass
 {
     public class Analaizer
     {
         private static int erposition = 0;
-        public static string expression = "";
+        public static string expression = "12+2*((3*4)+(11/5))";
         public static bool ShowMessage = true;
+        public Analaizer()
+        {
+            ContainerOperator.Add_Operators(new Operator("*", 3));
+            ContainerOperator.Add_Operators(new Operator("/", 3));
+            ContainerOperator.Add_Operators(new Operator("+", 2));
+            ContainerOperator.Add_Operators(new Operator("-", 2));
+            ContainerOperator.Add_Operators(new Operator("(", 1));
+        }
         public static bool CheckCurrency()
         {
             int n = 0;
@@ -19,7 +29,7 @@ namespace AnalaizerClass
 
             if (n == 0)
                 return true;
-            else if(n > 0)
+            else if (n > 0)
             {
                 erposition = expression.LastIndexOf('(');
                 return false;
@@ -30,12 +40,157 @@ namespace AnalaizerClass
                 return false;
             }
         }
-        //public static string Format() { }
-        //public static System.Collections.ArrayList CreateStack()
-        //{
+        public string Format()
+        {
+            string result = "";
+            string op = "*/+-";
+            int countNumOp = 0;
 
-        //}
-        public static string RunEstimate()
+            if (op.Contains(expression[expression.Length - 1]))
+                return "&Error 05";
+            if (expression.Length > 65536)
+                return "&Error 07";
+
+
+            for (int i = 0; i < expression.Length; ++i)
+            {
+                if (expression[i] != ' ')
+                    result += expression[i];
+
+                if (op.Contains(expression[i]))
+                    ++countNumOp;
+
+                if (Char.IsDigit(expression[i]) && !Char.IsDigit(expression[i + 1]) || op.Contains(expression[i]))
+                    result += ' ';
+                //else if (!op.Contains(expression[i]) && !Char.IsDigit(expression[i]) && expression[i] != ' ')
+                //{
+                //    string v = $"&Error 02 at <{i}>";
+                //    return v;
+                //}
+                else if (op.Contains(expression[i]) && op.Contains(expression[i + 1]))
+                {
+                    string v1 = $"&Error 04 at <{i + 1}>";
+                    return v1;
+                }
+            }
+
+            //for (int i = 0; i < result.Length; ++i)
+            //{
+            //    if (expression[i] == '/' && expression[i + 1] == '0' && expression[i + 2] == ' ')
+            //        return "&Error 9";
+            //}
+
+            string[] numbers = Regex.Split(expression, @"\D+");
+            foreach (string value in numbers)
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    int i = int.Parse(value);
+                    if (i <= -2147483648 || i >= 2147483647)
+                        return "&Error 6";
+                }
+            }
+
+            countNumOp += numbers.Length;
+            if (countNumOp > 30)
+                return "&Error 08";
+
+            return result;
+        }
+        public static System.Collections.ArrayList CreateStack(string ss)
+        {
+            System.Collections.ArrayList list = new System.Collections.ArrayList();
+            string input_string = "";
+            Stack<string> operators_stack = new Stack<string>();
+            string temp;
+            int ro;
+            int lo;
+            foreach (char c in input_string)
+            {
+                list.Add(c);
+            }
+
+            foreach (char c in ss)
+            {
+                if (Char.IsDigit(c))
+                {
+                    input_string += c;
+                    continue;
+                }
+                if (c == '+' || c == '-' || c == '*' || c == '/')
+                {
+                    input_string += " ";
+                    if (operators_stack.Count == 0)
+                    {
+                        operators_stack.Push(Convert.ToString(c));
+                        continue;
+                    }
+                    else if (operators_stack.Count != 0)
+                    {
+                        if (ContainerOperator.Find_Operator(operators_stack.Peek()).priority < ContainerOperator.Find_Operator(Convert.ToString(c)).priority)
+                        {
+                            operators_stack.Push(Convert.ToString(c));
+                            continue;
+                        }
+                    }
+                    if (operators_stack.Count != 0)
+                    {
+                        try
+                        {
+                            while (ContainerOperator.Find_Operator(operators_stack.Peek()).priority >= ContainerOperator.Find_Operator(Convert.ToString(c)).priority)
+                            {
+
+                                input_string += operators_stack.Pop();
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+                        if (operators_stack.Count == 0)
+                        {
+                            operators_stack.Push(Convert.ToString(c));
+                            continue;
+                        }
+                        else if (operators_stack.Count != 0)
+                        {
+                            if (ContainerOperator.Find_Operator(operators_stack.Peek()).priority < ContainerOperator.Find_Operator(Convert.ToString(c)).priority)
+                            {
+                                operators_stack.Push(Convert.ToString(c));
+                                continue;
+                            }
+                        }
+                    }
+                }
+                if (c == '(')
+                {
+
+                    operators_stack.Push(Convert.ToString(c));
+                }
+                if (c == ')')
+                {
+                    while (operators_stack.Peek() != "(")
+                    {
+                        input_string += " ";
+                        input_string += operators_stack.Pop();
+                    }
+                    operators_stack.Pop();
+                    continue;
+                }
+            }
+            while (operators_stack.Count != 0)
+            {
+                input_string += " ";
+                input_string += operators_stack.Pop();
+            }
+
+            foreach (char c in input_string)
+            {
+                list.Add(Convert.ToString(c));
+            }
+            return list;
+        }
+        public string RunEstimate()
         {
             string[] expTokens = expression.Split(' ');
             Stack<decimal> stack = new Stack<decimal>();
@@ -82,6 +237,26 @@ namespace AnalaizerClass
 
             return stack.Pop().ToString();
         }
-        //public static string Estimate() { }
+        public string Estimate()
+        {
+            if (CheckCurrency() == false)
+                throw new Exception($"Error 1 at <{erposition}>");
+
+            string result = Format();
+            if (result[0] == '&')
+                throw new Exception(result);
+
+            expression = result;
+            ArrayList stack = CreateStack(result);
+
+
+            expression = "";
+            foreach (var item in stack)
+            {
+                expression += item.ToString();
+            }
+            //Console.WriteLine(expression);
+            return RunEstimate();
+        }
     }
 }
---
+++ AnalaizerClass/Operator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnalaizerClass
+{
+    public class Operator
+    {
+        public int priority;
+        public string symbol;
+
+        public Operator(string symbol, int priority)
+        {
+            this.priority = priority;
+            this.symbol = symbol;
+        }
+    }
+    public class ContainerOperator
+    {
+        public static List<Operator> operators = new List<Operator>();
+        public static void Add_Operators(Operator op)
+        {
+            operators.Add(op);
+        }
+        public static Operator Find_Operator(string s)
+        {
+            foreach (var el in operators)
+            {
+                if (el.symbol == s)
+                {
+                    return el;
+                }
+            }
+            return null;
+        }
+    }
+}
